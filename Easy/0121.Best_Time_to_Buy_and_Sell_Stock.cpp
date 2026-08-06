13e88b1a68abd4a64306a905cfc894e13e261174
Praveen Kumar <praveenkumar42008@gmail.com>
2026-08-06 22:57:06 +0530

    Add 0121. Best Time to Buy and Sell Stock [Easy] (cpp)

--- Easy/0121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Easy/0121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -6,4 +6,15 @@
 // │  Synced by  : LeetSync Pro
 // └──────────────────────────────────────────────────────┘
 
-c
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int sell = 0;
+        for(int i = 0; i < prices.size(); i++){
+            buy = min(buy,prices[i]);
+            sell = max(sell,prices[i]- buy);
+        }
+        return sell;
+    }
+};
