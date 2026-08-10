ab86f4b8b5d1c78701ed74866386c290466728f6
Praveen Kumar <praveenkumar42008@gmail.com>
2026-08-10 21:21:29 +0530

    Create 242valid-anagram.cpp

---
+++ 242 Valid Anagram/242valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int arr[26] = {0};
+        for(int i = 0; i < s.length(); i++){
+            arr[s[i] - 'a'] += 1;
+            arr[t[i] - 'a'] -= 1;
+        }
+        for(int i : arr ){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
